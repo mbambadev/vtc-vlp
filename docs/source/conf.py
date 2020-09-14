98c4a96665d06077b9dc0ff23d0c6db68b5f78f4
Dolores Abernathy <mbambadev@outlook.com>
2020-09-14 02:10:39 +0100

    Some changes to try pass build to generate docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -89,7 +89,13 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark', 'sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest',
+              'sphinx.ext.intersphinx', 'sphinx.ext.todo',
+              'sphinx.ext.ifconfig', 'sphinx.ext.viewcode',
+              'sphinx.ext.inheritance_diagram',
+              'sphinx.ext.autosummary',
+              'recommonmark',
+              ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
