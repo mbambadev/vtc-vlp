9096830726f9f005031f084832d743eb7b99e013
Dolores Abernathy <mbambadev@outlook.com>
2020-09-14 04:20:37 +0100

    yml
    Took 4 minutes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@
 import os
 import sys
 import django
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'vtcvlp.settings'
 django.setup()
 
