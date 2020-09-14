21ce42ec06e5d2a959c4da8cf6efd028c7ddc37e
Dolores Abernathy <mbambadev@outlook.com>
2020-09-14 03:46:26 +0100

    yml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../home/vtcvlp/'))
+sys.path.insert(0, os.path.abspath('..'))
 from django.conf import settings
 import django
 django.setup()
@@ -118,3 +118,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+master_doc = 'index'
